813d57b9eaaac1b0feb7f41b34951a20dd05f694
koudelka <koudelkaa@gmail.com>
2011-02-24 18:38:42 +0100

    touchscreen calibration for gingerbread hack, thanks roggin

--- drivers/input/touchscreen/auo_ts.c
+++ drivers/input/touchscreen/auo_ts.c
@@ -17,9 +17,15 @@
 #define TS_DRIVER_NAME "auo-touch"
 
 #define AUO_TS_X_MIN             0
+#define AUO_TS_X_MAX             470
+#if defined (MIUI)
 #define AUO_TS_X_MAX             480
+#endif
 #define AUO_TS_Y_MIN             0
+#define AUO_TS_Y_MAX             790
+#if defined (MIUI)
 #define AUO_TS_Y_MAX             800
+#endif
 
 #define SLEEP_MODE_REG           0x70
 
